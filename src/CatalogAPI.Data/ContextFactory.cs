bccdb41bcec58ccbdef6a2b78c7775b716b56c3e
Marcio Torquato <marcio.limat@gmail.com>
2026-02-25 22:19:08 -0300

    alteração contextfactory

--- src/CatalogAPI.Data/ContextFactory.cs
+++ src/CatalogAPI.Data/ContextFactory.cs
@@ -1,16 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 
-namespace CatalogAPI.Data
-{
-    internal class ContextFactory : IDesignTimeDbContextFactory<CatalogApiDbContext>
-    {
-        public CatalogApiDbContext CreateDbContext(string[] args)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<CatalogApiDbContext>();
-            optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=MS_CatalogAPI;Integrated security=true");
+namespace CatalogAPI.Data;
 
-            return new CatalogApiDbContext(optionsBuilder.Options);
-        }
+internal class ContextFactory : IDesignTimeDbContextFactory<CatalogApiDbContext>
+{
+    public CatalogApiDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<CatalogApiDbContext>();
+        optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=MS_CatalogAPI;Integrated security=true");
+
+        return new CatalogApiDbContext(optionsBuilder.Options);
     }
 }
